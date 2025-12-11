22928e97c2362fa337be627648eca61b4883adb3
Mohammed Fazil Momin <mominmasty1214@gmail.com>
2025-12-11 14:35:28 +0530

    fix the tv endpoint

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)', 
   '/sign-up(.*)', 
-  '/api/webhooks(.*)'
+  '/api/webhooks(.*)',
+  '/api/tv(.*)'
 ]);
 
 export default clerkMiddleware(async (auth, request: NextRequest) => {
